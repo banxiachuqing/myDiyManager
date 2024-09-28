3e01557b4dee0d7904cae3657e987552765d82c4
Administrator <banxiachuqing@gmail.com>
2024-09-29 00:22:15 +0800

    更新菜单

---
+++ ruoyi-admin/src/main/resources/db/V20240925171000__add_manager_menu.sql
@@ -0,0 +1,4 @@
+-- 菜单 SQL
+INSERT INTO sys_menu (menu_id, menu_name, parent_id, order_num, path, component, query, route_name, is_frame, is_cache, menu_type, visible, status, perms,
+                      icon, create_by, create_time, update_by, update_time, remark)
+VALUES (2000, '任务中心', 0, 0, 'manager', null, null, '', 1, 0, 'M', '0', '0', null, 'druid', 'admin', '2024-09-29 00:11:42', '', null, '');
--- ruoyi-admin/src/main/resources/db/V20240926141000__add_step_log_menu.sql
+++ ruoyi-admin/src/main/resources/db/V20240926141000__add_step_log_menu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('小米刷步数运行日志', '2006', '1', 'log', 'step/log/index', 1, 0, 'C', '0', '0', 'step:log:list', '#', 'admin', sysdate(), '', null, '小米刷步数运行日志菜单');
+values('小米刷步数运行日志', '2000', '1', 'log', 'step/log/index', 1, 0, 'C', '0', '0', 'step:log:list', '#', 'admin', sysdate(), '', null, '小米刷步数运行日志菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
